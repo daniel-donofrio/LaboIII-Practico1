cd3f1dfb37ca1accd4e198afdb016cd28ed03f0f
Daniel D'Onofrio <danieldonofrio@live.com>
2024-04-21 22:16:10 -0300

    ejercicio 16

--- src/Ejercicios/Ejercicio16.java
+++ src/Ejercicios/Ejercicio16.java
@@ -1,7 +1,18 @@
 package Ejercicios;
+import java.util.Scanner;
 
 public class Ejercicio16 {
     public static void main(String[] args) {
-        
+        int numero;
+        Scanner numeroScanner = new Scanner(System.in);
+        System.out.print("Ingrese un numero decimal: ");
+        numero = numeroScanner.nextInt();
+        String binario = "";
+        while (numero > 0) {
+            int resto = numero % 2;
+            binario = resto + binario;
+            numero = numero / 2;
+        }
+        System.out.println("El número binario equivalente es: " + binario);
     }
 }
